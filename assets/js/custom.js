a22ac7b9e31469fa010db96d057cc1b2fa6b5312
Haridasan Shanmugam <hdasshp@gmail.com>
2023-01-31 11:08:48 +0530

    Initial Commit

---
+++ assets/js/custom.js
@@ -0,0 +1,22 @@
+jQuery(document).ready(function ($) {
+  $(window).on("load resize", function () {
+    if (this.matchMedia("(min-width: 768px)").matches) {
+      $(".customDropdown").on("mouseenter", () => {
+        $(".customDropdown > a").addClass("show")
+        $(".customDropdown > a").attr("aria-expanded", "true")
+        $(".customDropdown > div").attr("data-bs-popper", "none")
+        $(".customDropdown > div").addClass("show")
+      })
+
+      $(".customDropdown").on("mouseleave", () => {
+        $(".customDropdown > a").removeClass("show")
+        $(".customDropdown > a").attr("aria-expanded", "false")
+        $(".customDropdown > div").removeAttr("data-bs-popper", "none")
+        $(".customDropdown > div").removeClass("show")
+      })
+    }
+  })
+  $(".navbar-toggler").on("click", () => {
+    $("#menuHolder").slideToggle()
+  })
+})
